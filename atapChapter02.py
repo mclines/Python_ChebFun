a7273f413376e86b657272b2539e9af3e2c25f6c
Colton Mikes <colton@DESKTOP-LF8H22N.localdomain>
2018-09-04 00:46:08 -0500

    Updated Chapter02 material.

--- atapChapter02.py
+++ atapChapter02.py
@@ -44,7 +44,8 @@ A simple step function for use in examples throughout the rest of chapter 02.
   return result
 
 fig,b = plt.subplots()
+grid = np.linspace(-1,1,50)
 
-b.plot(XX,f(XX)) 
+b.plot(grid,f(grid)) 
 
 plt.savefig('figures/ch02/figure03.png')
